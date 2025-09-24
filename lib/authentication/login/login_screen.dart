561810420aa88b87b6401b5905f8c27f45884859
Moamen0 <momenabdallah306@gmail.com>
2025-09-24 03:59:04 +0300

    finish splash screen

--- lib/authentication/login/login_screen.dart
+++ lib/authentication/login/login_screen.dart
@@ -1,5 +1,7 @@
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:eventlyapp/Home%20Screen/tabs/widgets/custom_elevated_button.dart';
 import 'package:eventlyapp/Home%20Screen/tabs/widgets/custom_textformfiled.dart';
+import 'package:eventlyapp/Providers/app_language_provider.dart';
 import 'package:eventlyapp/generated/l10n.dart';
 import 'package:eventlyapp/utils/app_assets.dart';
 import 'package:eventlyapp/utils/app_color.dart';
@@ -7,6 +9,7 @@ import 'package:eventlyapp/utils/app_routes.dart';
 import 'package:eventlyapp/utils/app_style.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
@@ -23,11 +26,34 @@ class _LoginScreenState extends State<LoginScreen> {
       TextEditingController(text: "Moamen@gmail.com");
   TextEditingController passwordController =
       TextEditingController(text: "123456");
+  int? _getCurrentLanguageValue(String language) {
+    switch (language) {
+      case "en":
+        return 0;
+      case "ar":
+        return 1;
+      default:
+        return null;
+    }
+  }
+
+  String _getLanguageFromValue(int? value) {
+    switch (value) {
+      case 0:
+        return "en";
+      case 1:
+        return "ar";
+      default:
+        return "en";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     var hieght = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+    var languageProvider = Provider.of<AppLanguageProvider>(context);
+
     return Scaffold(
       body: SafeArea(
           child: Padding(
@@ -178,6 +204,27 @@ class _LoginScreenState extends State<LoginScreen> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: hieght * 0.02,
+              ),
+              AnimatedToggleSwitch<int?>.rolling(
+                iconOpacity: 100,
+                allowUnlistedValues: true,
+                styleAnimationType: AnimationType.onHover,
+                current: _getCurrentLanguageValue(languageProvider.appLanguage),
+                values: const [0, 1],
+                onChanged: (i) {
+                  final newLanguage = _getLanguageFromValue(i);
+                  languageProvider.changeLanguage(newLanguage);
+                },
+                iconBuilder: languageIconBuilder,
+                style: ToggleStyle(
+                    backgroundColor: Colors.transparent,
+                    borderColor: AppColor.primaryLightColor,
+                    borderRadius: BorderRadius.circular(25),
+                    indicatorColor: AppColor.primaryLightColor),
+                borderWidth: 3.0,
+              ),
             ],
           ),
         ),
@@ -193,4 +240,46 @@ class _LoginScreenState extends State<LoginScreen> {
       );
     }
   }
+
+  Widget languageIconBuilder(int? value, bool foreground) {
+    return Container(
+      child: Center(
+        child: flagByValue(value),
+      ),
+    );
+  }
+
+  Widget iconBuilder(int value) {
+    return languageIconBuilder(value, false);
+  }
+
+  Widget rollingIconBuilder(int? value, bool foreground) {
+    return Container(
+      child: Center(
+        child: flagByValue(value),
+      ),
+    );
+  }
+
+  Color colorBuilder(int value) => switch (value) {
+        0 => Colors.blueAccent,
+        1 => Colors.red,
+        _ => AppColor.primaryLightColor,
+      };
+
+  Widget flagByValue(int? value) => switch (value) {
+        0 => Flag(
+            Flags.united_states_of_america,
+            size: 28,
+          ),
+        1 => Flag(
+            Flags.egypt,
+            size: 28,
+          ),
+        _ => Icon(
+            Icons.language,
+            size: 28,
+            color: AppColor.primaryLightColor,
+          ),
+      };
 }
